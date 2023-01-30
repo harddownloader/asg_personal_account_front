527acc48849e36fba8b9f88e2383c4823e828cae
harddownloader <serafimdev@gmail.com>
2023-01-30 22:28:47 +0200

    added Layout for Registration page

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,7 +1,16 @@
-export default function RegistrationPage() {
+import { ReactElement } from "react"
+import { Layout } from "@/components/Layout/Layout"
+
+function RegistrationPage() {
     return (
         <>
             <h1>Registration</h1>
         </>
     )
 }
+
+RegistrationPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
+
+export default RegistrationPage
