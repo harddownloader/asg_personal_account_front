bbc78142798ebe7f41d5aae9185e955bc31b5f37
harddownloader <serafimdev@gmail.com>
2023-01-30 22:28:57 +0200

    added Layout for Login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,16 @@
-export default function LoginPage() {
+import { ReactElement } from "react"
+import { Layout } from "@/components/Layout/Layout"
+
+function LoginPage() {
     return (
      <>
         <h1>Login</h1>
      </>
     )
 }
+
+LoginPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
+
+export default LoginPage
