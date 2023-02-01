807182e241b6908d514b634f8f8f7f517b85c65c
harddownloader <serafimdev@gmail.com>
2023-02-01 16:17:17 +0200

    added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/img/favicon/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
