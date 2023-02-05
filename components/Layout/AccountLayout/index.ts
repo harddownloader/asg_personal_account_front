9bc56689f71f0ef95aeeaaea39358b22234a8b00
harddownloader <serafimdev@gmail.com>
2023-02-05 15:47:15 +0200

    added Layouts - for authenticated user and not

--- components/Layout/AccountLayout/index.ts
+++ components/Layout/AccountLayout/index.ts
@@ -1,4 +1,2 @@
 export type { AccountLayoutProps } from "./AccountLayout"
 export { AccountLayout } from "./AccountLayout"
-export type { AuthLayoutProps } from "./AuthLayout"
-export { AuthLayout } from "./AuthLayout"
