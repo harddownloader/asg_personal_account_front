909f7375c42d58e675526a549cb9c012ede4b7b6
harddownloader <serafimdev@gmail.com>
2023-02-10 02:48:08 +0200

    added auth and theme providers && implement layouts logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 import '@/styles/globals.scss'
+import React, { ReactElement, ReactNode } from "react"
+import { NextPage } from "next"
 import type { AppProps } from 'next/app'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
+import themes from '@/lib/themes'
+import { AuthProvider } from '@/lib/auth'
 
-const theme = createTheme()
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
-export default function App({ Component, pageProps }: AppProps) {
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
+
+  const customization = {
+    fontFamily: `'Roboto', sans-serif`,
+    borderRadius: '0'
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+    <ThemeProvider theme={themes(customization)}>
+      <AuthProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </AuthProvider>
     </ThemeProvider>
   )
 }
