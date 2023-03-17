3856152627b50e8eb9bbcc6624f33e212864a562
harddownloader <serafimdev@gmail.com>
2023-03-17 02:17:56 +0200

    update next config: add images settings

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
+const apiURL = new URL(process.env.NEXT_PUBLIC_API_URI)
+const allowedImageDomains = process.env.NEXT_PUBLIC_ALLOWED_IMAGE_DOMAINS
+  ? process.env.NEXT_PUBLIC_ALLOWED_IMAGE_DOMAINS.split(",")
+  : []
+const imageConversionFormats = process.env.NEXT_PUBLIC_IMAGE_CONVERSION_FORMATS
+  ? process.env.NEXT_PUBLIC_IMAGE_CONVERSION_FORMATS.split(",")
+  : []
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [apiURL.hostname, ...allowedImageDomains],
+    formats: imageConversionFormats,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    dangerouslyAllowSVG: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   webpack(config, { isServer }) {
     config.module.rules.push({
